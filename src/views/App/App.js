727687743f0aa9b9594db1e20d08759fdf9e55cc
Adam <adam@Mac-2.local>
2019-12-18 08:02:45 -0600

    Remove extraneous files

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -4,19 +4,6 @@ import './App.css';
 
 function App() {
   return (
- feature/quiz-questions
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
