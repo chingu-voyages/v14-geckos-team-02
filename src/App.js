9e1e85cda9c6b2ccf3e93d1596c4c3c1416de15f
upluke <lukeforadream@gmail.com>
2019-12-18 23:47:59 -0800

    Quiz&MultipleChoice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Quiz from './Quiz'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          V14-geckos-team-02
-        </a>
-      </header>
+    
+
+        <Quiz />
+   
     </div>
   );
 }
