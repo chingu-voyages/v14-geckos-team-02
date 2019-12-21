12c4fed3c6d2ea65828a3fcf71c3068bbd749675
Adam <adam@Mac-2.local>
2019-12-21 10:27:33 -0600

    Fix syntax errors in Footer.js

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -1,7 +1,9 @@
-import react,{useState} from 'react';
+import React,{useState} from 'react';
 
-export default = () => {
-  <div class="footerAS">
-    coded by Chingu Cohort 14 Gecko Team 2
-  </div>
+export default () => {
+  return(
+    <div class="footerAS">
+      coded by Chingu Cohort 14 Gecko Team 2
+    </div>
+  )
 };
