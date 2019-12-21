e3a0e7c70432ba744baa9b200417f4edcf383b29
Adam <adam@Mac-2.local>
2019-12-21 11:59:42 -0600

    Add props to pass homeClick method to Header Component

--- src/views/Header/Header.js
+++ src/views/Header/Header.js
@@ -1,12 +1,12 @@
 import React,{useState} from 'react';
 
-export default () => {
+export default (props) => {
   return(
     <div class="headerAS">
       <div class="titleAS">
         Data Structures and Algorithms
       </div>
-      <div class="home-iconAS" data-toggle="tooltip" title="Home Screen">
+      <div class="home-iconAS" data-toggle="tooltip" title="Home Screen" onClick={props.homeClick}>
         <img src="HomeIcons/icons8-home-page-50.png" alt="home icon"></img>
       </div>
     </div>
