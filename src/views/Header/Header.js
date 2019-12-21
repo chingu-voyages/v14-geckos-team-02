d1a90518806e3bc15e85b17dea245268b45b233c
Adam <adam@Mac-2.local>
2019-12-21 10:01:10 -0600

    Modify Header.js. Add return HTML to export default

--- src/views/Header/Header.js
+++ src/views/Header/Header.js
@@ -1,3 +1,15 @@
 import React,{useState} from 'react';
 
-export default () => {};
+export default () => {
+
+  return(
+    <div class="headerAS">
+      <div class="titleAS">
+        Data Structures and Algorithms
+      </div>
+      <div class="home-iconAS" data-toggle="tooltip" title="Home Screen">
+        <img src="HomeIcons/icons8-home-page-50.png" alt="home icon"></img>
+      </div>
+    </div>
+  )
+};
