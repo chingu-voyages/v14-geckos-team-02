3f24ad9a1bd3d1c0ee081e01609dd6005470d45e
upluke <lukeforadream@gmail.com>
2019-12-24 08:37:44 -0800

    updateQuiz&Mul

--- src/views/MultipleChoice/MultipleChoice.js
+++ src/views/MultipleChoice/MultipleChoice.js
@@ -1,25 +1,59 @@
-import React from 'react'
+import React from "react";
 
-export default ({question, choices, checkedOption,handleChecked, id})=>{
-
-    const myChoices=()=>{
-        return(
-            <div key={id}>
-                <label><input type="radio" checked={checkedOption===`${choices[0]}`} onChange={(e)=>handleChecked(e.target.value)} value={choices[0]}/> {choices[0]}</label>
-                <label><input type="radio" checked={checkedOption===`${choices[1]}`} onChange={(e)=>handleChecked(e.target.value)} value={choices[1]}/> {choices[1]}</label>
-                <label><input type="radio" checked={checkedOption===`${choices[2]}`} onChange={(e)=>handleChecked(e.target.value)} value={choices[2]}/> {choices[2]}</label>
-                <label><input type="radio" checked={checkedOption===`${choices[3]}`} onChange={(e)=>handleChecked(e.target.value)} value={choices[3]}/> {choices[3]}</label>
-            </div>
-       
-        )
-    }
-    return(
-        <div>
-            <h2>{question}</h2>
-            {myChoices()}
-
-        
-        </div>
-    )
-}
-
+export default ({
+  question,
+  choices,
+  checkedOption,
+  handleChecked,
+  id,
+  count
+}) => {
+  const myChoices = () => {
+    return (
+      <div key={id}>
+        <label>
+          <input
+            type="radio"
+            checked={checkedOption === `${choices[0]}`}
+            onChange={e => handleChecked(e.target.value, count)}
+            value={choices[0]}
+          />{" "}
+          {choices[0]}
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={checkedOption === `${choices[1]}`}
+            onChange={e => handleChecked(e.target.value, count)}
+            value={choices[1]}
+          />{" "}
+          {choices[1]}
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={checkedOption === `${choices[2]}`}
+            onChange={e => handleChecked(e.target.value, count)}
+            value={choices[2]}
+          />{" "}
+          {choices[2]}
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={checkedOption === `${choices[3]}`}
+            onChange={e => handleChecked(e.target.value, count)}
+            value={choices[3]}
+          />{" "}
+          {choices[3]}
+        </label>
+      </div>
+    );
+  };
+  return (
+    <div>
+      <h2>{question}</h2>
+      {myChoices()}
+    </div>
+  );
+};
--- src/views/Quiz/Quiz.js
+++ src/views/Quiz/Quiz.js
@@ -1,47 +1,49 @@
 import React,{useState} from 'react'
 import MultipleChoice from '../MultipleChoice/MultipleChoice';
+import {qBank} from '../../questionBank'
 
 
 
-export default ()=>{
-    const [QuestionDemo, setQuestionDemo]=useState({
-        question:"What is the best use of hash tables?",
-        choices:["find max/min","sorting","search or lookup","traverse"],
-        answer:"search or lookup", 
-        checkedOption:'',
-        id:"123"})
-    let [score,setScore]=useState(0) // countering score
-    let [value, setValue]=useState() // value that user select
-
-    const handleChecked=(value)=>{ 
-        setQuestionDemo({...QuestionDemo,checkedOption:value}) //enable radio btn respond to user's selection
-        setValue(value) // set value equal to user's selected value
-    }
-
-    const handleClicked=()=>{ // by clicking Next btn, answer gets va 
-        if(QuestionDemo.answer===value){ 
-            setScore(score+=1)
-       }
-        alert(`Your current score is ${score}`)
-    }
-    console.log(score)
-    return(
-    
-
-        <div className="container">
-            <h1>Quiz Board</h1> 
-            <div>
-                <MultipleChoice 
-                question={QuestionDemo.question} 
-                choices={QuestionDemo.choices} 
-                answer={QuestionDemo.answer} 
-                checkedOption={QuestionDemo.checkedOption} 
-                handleChecked={handleChecked}
-                id={QuestionDemo.id}  />
-            </div>
-            <button   >Previous</button>
-            <button onClick={()=>handleClicked()} >Next</button>
-            
+export default () => {
+    const [questions, setQuestions] = useState([...qBank]);
+    let [score, setScore] = useState(0); // countering score
+    let [value, setValue] = useState(); // value that user select
+    let [count, setCount] = useState(0); // track on the number of questions
+  
+    const handleChecked = (value, count) => {
+      const newQuestions = [...questions];
+      newQuestions[count].checkedOption = value;
+      setQuestions([...newQuestions]);
+      setValue(value); // set value equal to user's selected value
+    };
+  
+    const handleClicked = () => {
+      if (questions[count].correct === value) {
+        setScore((score += 1));
+      }
+      alert(`Your current score is ${score}`);
+      const newCount = count < questions.length ? count + 1 : count;
+      setCount(newCount);
+    };
+    // console.log(score)
+    return (
+      <div className="container">
+        <h1>Quiz Board</h1>
+        <div>
+          <MultipleChoice
+            question={questions[count].question}
+            choices={questions[count].answers}
+            answer={questions[count].correct}
+            checkedOption={questions[count].checkedOption}
+            handleChecked={handleChecked}
+            id={questions[count].questionId}
+            count={count}
+          />
         </div>
-    )
-}
+  
+        <button>Previous</button>
+        <button onClick={() => handleClicked()}>Next</button>
+      </div>
+    );
+  };
+  
