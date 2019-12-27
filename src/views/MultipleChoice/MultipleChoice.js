2ce2efbdf565795fbe6cda45bf9c5e13709b571d
upluke <lukeforadream@gmail.com>
2019-12-27 15:23:31 -0800

    quizMulUpdate

--- src/views/MultipleChoice/MultipleChoice.js
+++ src/views/MultipleChoice/MultipleChoice.js
@@ -6,54 +6,29 @@ export default ({
   checkedOption,
   handleChecked,
   id,
+  answer,// Test only
   count
 }) => {
-  const myChoices = () => {
-    return (
-      <div key={id}>
-        <label>
-          <input
-            type="radio"
-            checked={checkedOption === `${choices[0]}`}
-            onChange={e => handleChecked(e.target.value, count)}
-            value={choices[0]}
-          />{" "}
-          {choices[0]}
-        </label>
-        <label>
-          <input
-            type="radio"
-            checked={checkedOption === `${choices[1]}`}
-            onChange={e => handleChecked(e.target.value, count)}
-            value={choices[1]}
-          />{" "}
-          {choices[1]}
-        </label>
-        <label>
-          <input
-            type="radio"
-            checked={checkedOption === `${choices[2]}`}
-            onChange={e => handleChecked(e.target.value, count)}
-            value={choices[2]}
-          />{" "}
-          {choices[2]}
-        </label>
-        <label>
-          <input
-            type="radio"
-            checked={checkedOption === `${choices[3]}`}
-            onChange={e => handleChecked(e.target.value, count)}
-            value={choices[3]}
-          />{" "}
-          {choices[3]}
-        </label>
-      </div>
-    );
-  };
-  return (
-    <div>
-      <h2>{question}</h2>
-      {myChoices()}
-    </div>
-  );
-};
+    const myChoices=choices.map(c=>{
+        return <div >
+            <label key={id}>
+                <input
+              type="radio"
+              checked={checkedOption === `${c}`}
+                  onChange={e => handleChecked(e.target.value, count)}
+                value={c}
+             />{""}
+            {c}
+         </label>
+         </div>
+       })
+       
+       return (
+         <div>
+           <h2>{question}</h2> 
+           <span style={{color:'green'}}>(Answer: {answer})</span> {/* Test only */}
+           {myChoices}
+         </div>
+       );
+     };
+     
--- src/views/Quiz/Quiz.js
+++ src/views/Quiz/Quiz.js
@@ -1,23 +1,23 @@
 import React,{useState} from 'react'
 import MultipleChoice from '../MultipleChoice/MultipleChoice';
-import {qBank} from '../../questionBank'
 
 
+var qBank=require('../../quiz.json')
 
 export default () => {
     const [questions, setQuestions] = useState([...qBank]);
-    let [score, setScore] = useState(0); // countering score
+    let [score, setScore] = useState(0); // counting score
     let [value, setValue] = useState(); // value that user select
     let [count, setCount] = useState(0); // track on the number of questions
   
-    const handleChecked = (value, count) => {
+    const handleCheck = (value, count) => {
       const newQuestions = [...questions];
       newQuestions[count].checkedOption = value;
       setQuestions([...newQuestions]);
       setValue(value); // set value equal to user's selected value
     };
   
-    const handleClicked = () => {
+    const handleClickNext = () => {
       if (questions[count].correct === value) {
         setScore((score += 1));
       }
@@ -25,24 +25,32 @@ export default () => {
       const newCount = count < questions.length ? count + 1 : count;
       setCount(newCount);
     };
-    // console.log(score)
+   
+    const handleClickPrevious= () => {
+    
+        setScore((score -= 1));
+        const newCount = count < questions.length ? count - 1 : count;
+        setCount(newCount);
+      };
+
     return (
       <div className="container">
         <h1>Quiz Board</h1>
+        <span style={{color:"red"}}>(You current score is: {score})</span>  {/* Test only */}
         <div>
           <MultipleChoice
             question={questions[count].question}
             choices={questions[count].answers}
             answer={questions[count].correct}
             checkedOption={questions[count].checkedOption}
-            handleChecked={handleChecked}
+            handleChecked={handleCheck}
             id={questions[count].questionId}
             count={count}
           />
         </div>
   
-        <button>Previous</button>
-        <button onClick={() => handleClicked()}>Next</button>
+        <button onClick={() => handleClickPrevious()}>Previous</button>
+        <button onClick={() => handleClickNext()}>Next</button>
       </div>
     );
   };
