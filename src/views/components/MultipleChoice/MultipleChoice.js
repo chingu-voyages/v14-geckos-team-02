af39527c34d8dffd6a679d2713cfd3937a944c29
upluke <lukeforadream@gmail.com>
2020-01-01 17:20:43 -0800

    removeKeyWarning

--- src/views/components/MultipleChoice/MultipleChoice.js
+++ src/views/components/MultipleChoice/MultipleChoice.js
@@ -8,12 +8,12 @@ export default ({
   answer,// Test only
 }) => {
        return (
-         <div>
+         <div key={id}>
            <h2>{question}</h2> 
            <span style={{color:'green'}}>(Answer: {answer})</span> {/* Test only */}
-                {choices.map(choice => (
-                  <div>
-                    <label key={id}>
+                {choices.map((choice,i) => (
+                  <div key={i}>
+                    <label>
                       <input type="button" name={id} value={choice} onClick={e=>handleCheck(e.target.value)} />
                     </label>
                   </div>
