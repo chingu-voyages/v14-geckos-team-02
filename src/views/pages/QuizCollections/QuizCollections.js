ee111beef2ecff97e5c66bc639bf4b165623e639
upluke <lukeforadream@gmail.com>
2020-01-02 23:52:54 -0800

    css&Data

--- src/views/pages/QuizCollections/QuizCollections.js
+++ src/views/pages/QuizCollections/QuizCollections.js
@@ -1,31 +1,14 @@
-import React,{useState} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
 import './QuizCollections.scss';
+import sujectData from './subject-data'
 
 export default ()=>{
-   const [state]=useState([
-     {title:'Binary Search Tree',
-      link:'/big-o',   // needs to be updated with the correct url
-      image:'https://upload.wikimedia.org/wikipedia/commons/9/9b/Binary_search_tree_example.gif',
-      text:'In computer science, binary search trees, sometimes called ordered or sorted binary trees, are a particular type of container: a data structure that stores "items" in memory.'
-    },
-     {title:'Big O Notation',
-      link:'/big-o',
-      image:'http://cse.csusb.edu/dick/cs202/bigO.gif',
-     text:'Big O notation is a mathematical notation that describes the limiting behavior of a function when the argument tends towards a particular value or infinity.'
-    },
-     {title:'Sorting (Coming soon)',
-     link:'/big-o', // needs to be updated with the correct url
-     image:'https://miro.medium.com/max/500/0*khkSk6LZGgtqnjev.jpg',
-     text:'A Sorting Algorithm is used to rearrange a given array or list elements according to a comparison operator on the elements.'}
-   ])
-   
-    
     return(
-        <div style={{display:"flex"}}>
-          {state.map(s=>(
+        <div className="colections" >
+          {sujectData.map(s=>(
             <div className="card" key={s.title} >
-                <header style={{backgroundImage:`url(${s.image})`,backgroundSize: "cover",backgroundRepeat:"no-repeat"}} className="card-header" />
+                <header style={{backgroundImage:`url(${s.image})`}} className="card-header" />
                 <div className="card-body">
                     <h2>{s.title}</h2>
                     <p className="body-content">{s.text}</p>
---
+++ src/views/pages/QuizCollections/subject-data.js
@@ -0,0 +1,20 @@
+ const sujectData=[
+     
+    {title:'Binary Search Tree',
+     link:'/big-o',   // needs to be updated with the correct url
+     image:'https://upload.wikimedia.org/wikipedia/commons/9/9b/Binary_search_tree_example.gif',
+     text:'In computer science, binary search trees, sometimes called ordered or sorted binary trees, are a particular type of container: a data structure that stores "items" in memory.'
+   },
+    {title:'Big O Notation',
+     link:'/big-o',
+     image:'http://cse.csusb.edu/dick/cs202/bigO.gif',
+    text:'Big O notation is a mathematical notation that describes the limiting behavior of a function when the argument tends towards a particular value or infinity.'
+   },
+    {title:'Sorting (Coming soon)',
+    link:'/big-o', // needs to be updated with the correct url
+    image:'https://miro.medium.com/max/500/0*khkSk6LZGgtqnjev.jpg',
+    text:'A Sorting Algorithm is used to rearrange a given array or list elements according to a comparison operator on the elements.'
+   }
+]
+
+export default sujectData
