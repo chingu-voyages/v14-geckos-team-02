4aaf112a1480e8f9a0764d135ddc212258f7ad12
Sal Tamay <sal.tamay@outlook.com>
2020-01-03 14:19:43 -0500

    style: Update quiz card styles

--- src/views/pages/QuizCollections/QuizCollections.js
+++ src/views/pages/QuizCollections/QuizCollections.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import './QuizCollections.scss';
-import sujectData from './subject-data'
 
-export default ()=>{
+import './QuizCollections.scss';
+import quizSubjects from './subject-data'
+
+export default () => {
     return(
-        <div className="colections" >
-          {sujectData.map(s=>(
-            <div className="card" key={s.title} >
-                <header style={{backgroundImage:`url(${s.image})`}} className="card-header" />
+        <div className="collections" >
+          {quizSubjects.map(quizSubject=>(
+            <div className="card" key={quizSubject.title} >
+                <div className="card-header">
+                    <img className="card-image" src={`${quizSubject.image}`} alt="" />
+                </div>
                 <div className="card-body">
-                    <h2>{s.title}</h2>
-                    <p className="body-content">{s.text}</p>
+                    <h2>{quizSubject.title}</h2>
+                    <p className="body-content">{quizSubject.text}</p>
                 </div>
                 <div className="btn-display">
                     <button className="button-primary">
-                        <Link to={s.link}>Take Quiz</Link> 
+                        <Link to={quizSubject.link}>Take Quiz</Link> 
                     </button>
                 </div>  
             </div>
             ))}
         </div>
-      
     )
 }
