027a0ba12a9348d1167cc4b89be6cdb55a787a1b
Adam <adam@Mac-2.local>
2020-01-13 11:39:10 -0600

    1.Modify Quiz.js 2.Insert ProgressBar component

--- src/views/components/Quiz/Quiz.js
+++ src/views/components/Quiz/Quiz.js
@@ -3,7 +3,8 @@ import './Quiz.scss';
 
 import Question from '../Question/Question';
 import MultipleChoice from '../MultipleChoice/MultipleChoice';
-import Results from '../Results/Results'
+import Results from '../Results/Results';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
 import QUIZ_DATA from '../../../quiz-data';
 
@@ -12,23 +13,23 @@ const Quiz = () => {
     let [score, setScore] = useState(0); // counting score
     let [value, setValue] = useState(); // value that user select
     let [count, setCount] = useState(0); // track on the number of questions
-    
+
 
     const handleCheck = (value) => {
       setValue(value); // set value equal to user's selected value
     };
-  
+
     const handleClickNext = () => {
       if (questions[count].answer === value) {
         setScore((score += 1));
       }
       alert(`Your current score is ${score}`);
       let newCount = count < questions.length ? count + 1 : count;
-      
+
       setCount(newCount);
     };
     console.log(count, "---", questions.length)
-    
+
     return (
       <div  >
       {(count===questions.length)?
@@ -38,6 +39,7 @@ const Quiz = () => {
         <span style={{color:"red"}}>(Your current score is: {score})</span>  {/* Test only */}
         <div>
           <Question question={questions[count].question} />
+          <ProgressBar />
           <MultipleChoice
               choices={questions[count].multipleChoices}
               answer={questions[count].answer}
@@ -48,10 +50,9 @@ const Quiz = () => {
         </div>
       </div>
        }
-        
+
       </div>
     );
   };
 
   export default Quiz;
-  
