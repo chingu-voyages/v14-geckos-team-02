90b77e02fe652df085c12953af6be7149351696d
Sal Tamay <sal.tamay@outlook.com>
2020-01-20 00:44:16 -0500

    fix: Websocket error and refactor

--- src/views/pages/QuizPage/QuizPage.js
+++ src/views/pages/QuizPage/QuizPage.js
@@ -1,29 +1,31 @@
-import React,{useState} from 'react'
- 
-import Results from '../../components/Results/Results'
-import Quiz from '../../components/Quiz/Quiz'
+import React, { useState } from 'react';
+
+import Results from '../../components/Results/Results';
+import Quiz from '../../components/Quiz/Quiz';
 import QUIZ_DATA from '../../../quiz-data';
 
-export default (props)=>{
+export default props => {
   const [questions] = useState(QUIZ_DATA[props.match.params.id].questions);
   let [score, setScore] = useState(0); // counting score
   let [value, setValue] = useState(); // value that user select
   let [count, setCount] = useState(0); // track on the number of questions
-  
+
   const clearActiveItem = () => {
     if (document.querySelector('.active-item')) {
-      document.querySelector('.active-item .checkmark').innerHTML = '<i class="far fa-circle">';
+      document.querySelector('.active-item .checkmark').innerHTML =
+        '<i class="far fa-circle">';
       document.querySelector('.active-item').classList.remove('active-item');
-    } 
-  }
+    }
+  };
 
-  const handleCheck = (event) => {
+  const handleCheck = event => {
     setValue(event.target.dataset.value); // set value equal to user's selected value
-    
+
     clearActiveItem();
-    
+
     event.target.classList.add('active-item');
-    event.target.querySelector('.checkmark').innerHTML = '<i class="fas fa-check-circle"></i>';
+    event.target.querySelector('.checkmark').innerHTML =
+      '<i class="fas fa-check-circle"></i>';
     document.querySelector('.next-button').disabled = false;
   };
 
@@ -35,21 +37,22 @@ export default (props)=>{
       let newCount = count < questions.length ? count + 1 : count;
 
       clearActiveItem();
-      
+
       setCount(newCount);
     }
   };
-    console.log(props.match.params.id)
-    return(
-        <div>
-        {(count===questions.length)?
-            <Results score={score} numOfQuestions={questions.length}/>:
-            <Quiz
-                questions={questions}
-                count={count}
-                handleCheck={handleCheck} 
-                handleClickNext={handleClickNext} 
-            />}
-        </div>
-    )
-}
+  return (
+    <div>
+      {count === questions.length ? (
+        <Results score={score} numOfQuestions={questions.length} />
+      ) : (
+        <Quiz
+          questions={questions}
+          count={count}
+          handleCheck={handleCheck}
+          handleClickNext={handleClickNext}
+        />
+      )}
+    </div>
+  );
+};
